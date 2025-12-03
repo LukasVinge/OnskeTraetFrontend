ca8a772abd136983687405d834087ce6df72ba2c
Lukas Vinge <lukashvinge@gmail.com>
2025-12-03 12:26:49 +0100

    deploy nu?

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import { supabaseServer } from 'lib/supabase-server'
-
-export default async function DashboardPage() {
-  const supabase = supabaseServer()
-  const { data: { user } } = await supabase.auth.getUser()
-
-  return <div>Welcome {user?.email}</div>
-}
